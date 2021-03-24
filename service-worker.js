8a514e0dca44721b94bcd9e331415778e460a5bb
aishaar <aishaar283@gmail.com>
2021-03-24 12:03:37 +0400

    sixth commit

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,7 @@ self.addEventListener('install', (e) => {
    );
 });
 
-self.addEventListener('fetch', function (e) {
+/*self.addEventListener('fetch', function (e) {
     e.respondWith(
         //check if the cache has the file
         caches.match(e.request).then(function (r) {
@@ -26,5 +26,33 @@ self.addEventListener('fetch', function (e) {
             return r
         })
     );
-});
+});*/
+
+self.addEventListener('fetch', function (e) {​​
+
+    e.respondWith(
+
+        caches.match(e.request).then(function (r) {​​
+
+            // Download the file if it is not in the cache, 
+
+            return r || fetch(e.request).then(function (response) {​​
+
+                // add the new file to cache
+
+                return caches.open(cacheName).then(function (cache) {​​
+
+                    cache.put(e.request, response.clone());
+
+                    return response;
+
+                }​​);
+
+            }​​);
+
+        }​​)
+
+    );
+
+}​​);
 
